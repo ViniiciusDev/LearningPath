d8b478f2721c356aa524e445724f7b9d194a2530
Vinicius Gomes <ViniiciusGomes27@gmail.com>
2026-03-02 13:13:10 +0100

    API

---
+++ Pacotes/DayJS/script.js
@@ -0,0 +1,5 @@
+// Dessa forma retorna um objeto, e será necessário formatar.
+const now = dayjs()
+
+// A formatação podemos encontrar tudo na documentação do DayJS, e é bem simples de usar.
+console.log(now.format("DD/MM/YYYY - HH:mm"));
