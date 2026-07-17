d360cc5cb87ae0c1aa5ae0ab8c3cdb9b51754578
Vinicius Gomes <ViniiciusGomes27@gmail.com>
2026-07-17 10:41:10 +0200

    NodeJS Study

---
+++ NODEJS/api/src/server.js
@@ -0,0 +1,17 @@
+import http from "node:http"
+
+const server = http.createServer((request, response) => {
+    // Desestruturar o proprio método
+    const { method, url } = request
+
+    if (method === "GET" && url === "/products") {
+        return response.end("Lista de produtos")
+    }
+
+    if (method === "POST" && url === "/products") {
+        return response.writeHead(201).end("Produto Criado com Successo!")
+    }
+
+    return response.writeHead(404).end("Rota não encontrada!")
+})
+server.listen(3333)
